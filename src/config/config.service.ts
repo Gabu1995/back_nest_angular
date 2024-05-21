c730d887805562a01308ab54ca7e6c35bb79c8a2
Gabu1995 <gabrielcajas1995@hotmail.com>
2024-05-21 09:58:30 -0500

    Agregar datos a la base
    modificar cambiar etc

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -2,10 +2,22 @@ import { Injectable } from '@nestjs/common';
 import * as fs from 'fs'
 import {parse} from "dotenv"
 
+
 @Injectable()
 export class ConfigService {
     private readonly envConfig:{[key:string]:string}
     constructor(){
+        const env =process.env.NODE_ENV || 'development'
+        const envFilePath =`${__dirname}/../../.env.${env}`
+        const existsPath= fs.existsSync(envFilePath)
+            if(!existsPath){
+                console.log(`.env.${process.env.NODE_ENV} no existe`)
+                process.exit(0)
+            }
+            this.envConfig=parse(fs.readFileSync(envFilePath))
+        
+    
+       /*
         const isDevelopmentEnv=process.env.NODE_ENV !== 'production'
         if(isDevelopmentEnv){
             const envFilePath=__dirname +'/../../.env.development'
@@ -18,7 +30,7 @@ export class ConfigService {
         }
         else
         {
-            const envFilePath=__dirname +'/../../../.env.production'
+            const envFilePath=__dirname +'/../../.env.production'
             const existsPath = fs.existsSync(envFilePath)
             if(!existsPath){
                 console.log('.env.production no existe Production')
@@ -30,4 +42,12 @@ export class ConfigService {
     get(key: string):string{
         return this.envConfig[key];
     }
+}
+*/
+
+}
+
+get(key: string):string{
+    return this.envConfig[key];
+}
 }
