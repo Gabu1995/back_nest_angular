875cc5a3f510302aea2a8c77872e8c069f023858
Gabu1995 <gabrielcajas1995@hotmail.com>
2024-05-23 10:02:09 -0500

    clase 23/05/2024
    datos en la base: categoria

---
+++ src/modules/categoria/entities/categoria.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Categoria {
+    @PrimaryGeneratedColumn()
+    
+    id: number;
+
+    @Column()
+    nombre: string;           
+
+  
+
+    @Column()
+    detalle: string;
+}
